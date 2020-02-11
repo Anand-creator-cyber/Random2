2e65cfd69d9120a372b31048fa3af480e74b637c
Anand-creator-cyber <anand.subra8@gmail.com>
2020-02-11 20:52:52 +0530

    Commentary

--- sketch.js
+++ sketch.js
@@ -15,6 +15,7 @@ function setup(){
     engine = Engine.create();
     world = engine.world;
 
+    //A platform is being created and the background is being set
 
     ground = new Ground(600,height,1200,20);
     platform = new Ground(150, 305, 300, 170);
